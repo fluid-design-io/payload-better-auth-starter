7bd2c37b1089f19b8d691427176bfe61d46c3fe1
Oliver Pan <2216991777@qq.com>
2025-09-25 14:52:20 -0500

    fix blog DYNAMIC_SERVER_USAGE error

--- src/app/(frontend)/(public)/blog/[slug]/data.ts
+++ src/app/(frontend)/(public)/blog/[slug]/data.ts
@@ -1,18 +1,13 @@
-import { draftMode } from 'next/headers'
 import { cache } from 'react'
 
 import { getPayload } from '@/lib/payload/get-payload'
 
 export const queryPostsBySlug = cache(async ({ slug }: { slug: string }) => {
-  const { isEnabled: draft } = await draftMode()
-
   const payload = await getPayload()
 
   const result = await payload.find({
     collection: 'blog',
-    draft,
     limit: 1,
-    overrideAccess: draft,
     pagination: false,
     where: {
       slug: {
